ea9eab10d8e5a634feeadd201330ed36e80e1a34
jumorisse <64202414+jumorisse@users.noreply.github.com>
2021-07-27 11:46:02 +0200

    changed deploy url

--- 06_main.js
+++ 06_main.js
@@ -29,7 +29,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "261",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/261",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
