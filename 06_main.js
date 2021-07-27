8064924db6e3fe987a8344e147614bd014acda6a
jumorisse <64202414+jumorisse@users.noreply.github.com>
2021-07-27 11:34:06 +0200

    pilot ready for hosting

--- 06_main.js
+++ 06_main.js
@@ -28,14 +28,14 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            experimentID: "261",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/261",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
-            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+            deployMethod: "directLink",
+            contact_email: "jmorisse@uni-osnabrueck.de",
+            //prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
         progress_bar: {
